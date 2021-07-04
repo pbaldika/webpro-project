3f237eeadec4746d8b3d0c13fffdf1f6eadbc906
mhdrofiq <73174875+mhdrofiq@users.noreply.github.com>
2021-07-04 20:58:04 +0800

    Add files via upload

---
+++ assets/includes/breadcrumbs.php
@@ -0,0 +1,16 @@
+<?php { ?>
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Inner Page</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>Inner Page</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+<?php } ?>
---
+++ assets/includes/header.php
@@ -0,0 +1,54 @@
+<?php { ?>
+    <!-- ======= Top Bar ======= -->
+    <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+        <div class="container d-flex">
+        <div class="contact-info mr-auto">
+            <i class="icofont-envelope"></i> <a href="">contact@example.com</a>
+            <i class="icofont-phone"></i> +1 5589 55488 55
+        </div>
+        <div class="social-links">
+            <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+            <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+            <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        </div>
+        </div>
+    </div>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo mr-auto"><a href="index.php">SafeStay</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav class="nav-menu d-none d-lg-block">
+        <ul>
+            <li class="active"><a href="index.php">Home</a></li>
+            
+            <li><a href="package-list.php">Packages</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="drop-down"><a href="">Locations</a>
+            <ul>
+                <li class="drop-down"><a href="#">Kuala Lumpur</a>
+                <ul>
+                    <li><a href="#">Hilltop</a></li>
+                    <li><a href="#">Motel</a></li>
+                    <li><a href="#">Villas</a></li>
+                    <li><a href="#">Resort Worlds</a></li>
+                    <li><a href="#">Lake Place</a></li>
+                </ul>
+                </li>
+                <li><a href="#">Petaling Jaya</a></li>
+                <li><a href="#">Johor Bahru</a></li>
+                <li><a href="#"></a></li>
+            </ul>
+            </li>
+        </ul>
+        </nav><!-- .nav-menu -->
+        <a href="" class="get-started-btn scrollto">Admin Login</a>
+    </div>
+    </header>
+<?php } ?>
