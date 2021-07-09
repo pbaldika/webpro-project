eb54d7ca39294928408cbaf74eebc7547dff68bf
soordi30 <86918275+soordi30@users.noreply.github.com>
2021-07-09 18:53:00 +0800

    Update header.php

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -3,8 +3,8 @@
     <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
         <div class="container d-flex">
         <div class="contact-info mr-auto">
-            <i class="icofont-envelope"></i> <a href="">contact@example.com</a>
-            <i class="icofont-phone"></i> +1 5589 55488 55
+            <i class="icofont-envelope"></i> <a href="">safestay@gmail.com</a>
+            <i class="icofont-phone"></i> +60 4206 92119 66
         </div>
         <div class="social-links">
             <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
