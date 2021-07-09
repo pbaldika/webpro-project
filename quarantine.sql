c553e14dc8de63b78b8a1315c71201de05a5a037
soordi30 <86918275+soordi30@users.noreply.github.com>
2021-07-09 18:40:48 +0800

    Update quarantine.sql
    minor changes made using mySQL

--- quarantine.sql
+++ quarantine.sql
@@ -1,48 +1,141 @@
-/*
-CREATE TABLE "admin"
-(
-    "id" int(11) NOT NULL,
-    "username" varchar(100) DEFAULT NULL,
-    "password" varchar(100) DEFAULT NULL,
-    "updation_date" timestamp NULL DEFAULT NULL
-)
 
-CREATE TABLE "packages"
-(
-    "htl_id" int(11) NOT NULL,
-    "pkg_id" int(11) NOT NULL,
-    "pkg_name" varchar(200) DEFAULT NULL,
-    "pkg_type" varchar(150) DEFAULT NULL,
-    "pkg_price" int(11) DEFAULT NULL,
-    "pkg_features" varchar(255) DEFAULT NULL,
-    "pkg_details" mediumtext DEFAULT NULL,
-    "pkg_image" varchar(100) DEFAULT NULL,
-    "creation_date" timestamp NULL DEFAULT current_timestamp(),
-    "updation_date" timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-)
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE "booking"
-(
-    "booking_id" int(11) NOT NULL,
-    "pkg_id" int(11) DEFAULT NULL,
-    "user_email" varchar(100) DEFAULT NULL,
-    "in_date" varchar(100) DEFAULT NULL,
-    "out_date" varchar(100) DEFAULT NULL,
-    "booking_date" timestamp NULL DEFAULT current_timestamp(),
-    "status" int(11) DEFAULT NULL,
-    "updation_date" timestamp NULL DEFAULT NULL ON UPDATE current_timestamp() 
-)
-*/
 
-CREATE TABLE "hotels"
-(
-    "htl_id" int(11) NOT NULL,
-    "htl_name" varchar(200) DEFAULT NULL,
-    "htl_location" varchar(100) DEFAULT NULL,
-    "htl_image" varchar(100) DEFAULT NULL,
-    "creation_date" timestamp NULL DEFAULT current_timestamp(),
-    "updation_date" timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `update_date` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+CREATE TABLE `booking` (
+  `booking_id` int(11) NOT NULL,
+  `pkg_id` int(11) DEFAULT NULL,
+  `user_email` varchar(100) DEFAULT NULL,
+  `in_date` varchar(100) DEFAULT NULL,
+  `out_date` varchar(100) DEFAULT NULL,
+  `booking_date` timestamp NULL DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `update_date` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotels`
+--
+
+DROP TABLE IF EXISTS `hotels`;
+CREATE TABLE `hotels` (
+  `htl_id` int(11) NOT NULL,
+  `htl_name` varchar(200) DEFAULT NULL,
+  `htl_location` varchar(100) DEFAULT NULL,
+  `htl_image` varchar(100) DEFAULT NULL,
+  `creation_date` timestamp NULL DEFAULT NULL,
+  `update_date` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+DROP TABLE IF EXISTS `packages`;
+CREATE TABLE `packages` (
+  `htl_id` int(11) NOT NULL,
+  `pkg_id` int(11) NOT NULL,
+  `pkg_name` varchar(200) DEFAULT NULL,
+  `pkg_type` varchar(150) DEFAULT NULL,
+  `pkg_price` int(11) DEFAULT NULL,
+  `pkg_features` varchar(255) DEFAULT NULL,
+  `pkg_details` mediumtext DEFAULT NULL,
+  `pkg_image` varchar(100) DEFAULT NULL,
+  `creation_date` timestamp NULL DEFAULT NULL,
+  `update_date` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `hotels`
+--
+ALTER TABLE `hotels`
+  ADD PRIMARY KEY (`htl_id`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`pkg_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hotels`
+--
+ALTER TABLE `hotels`
+  MODIFY `htl_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 INSERT INTO "hotels" ("htl_id", "htl_name", "htl_location", "htl_image", "creation_date", "updation_date") VALUES
 (1, "Central Hotel KLIA", "KLIA Terminal 2", "portfolio-2.jpg", "2021-07-09 06:03:00", NULL),
