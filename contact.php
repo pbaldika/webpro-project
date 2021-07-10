0ecd6cd2a192a858b67a01e984aa041c78c71655
dhioridhola <86360923+dhioridhola@users.noreply.github.com>
2021-07-10 09:18:24 +0700

    Update contact.php
    Tinggal maps nya blom

--- contact.php
+++ contact.php
@@ -59,7 +59,7 @@
 
         <div class="section-title">
             <h2>Contact</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+            <p>Wherever you are, we are always by your side with just one click!</p>
         </div>
 
         <div class="row">
@@ -69,19 +69,19 @@
                 <div class="address">
                 <i class="icofont-google-map"></i>
                 <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <p>A108 Jalan Tun Abdul Rahman, Bukit Jalil, Kuala Lumpur, 535022</p>
                 </div>
 
                 <div class="email">
                 <i class="icofont-envelope"></i>
                 <h4>Email:</h4>
-                <p>info@example.com</p>
+                <p>safestay@gmail.com</p>
                 </div>
 
                 <div class="phone">
                 <i class="icofont-phone"></i>
                 <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
+                <p>+60 4206 92119 66</p>
                 </div>
 
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
