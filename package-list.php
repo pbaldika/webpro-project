387b1288645b223d11d11f68b76dac10e436fd45
pbaldika <68334626+pbaldika@users.noreply.github.com>
2021-07-10 20:15:06 +0700

    Update package-list.php

--- package-list.php
+++ package-list.php
@@ -52,10 +52,9 @@ include('assets/includes/config.php');
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2>Inner Page</h2>
           <ol>
             <li><a href="index.php">Home</a></li>
-            <li>Inner Page</li>
+            <li>Package List</li>
           </ol>
         </div>
 
