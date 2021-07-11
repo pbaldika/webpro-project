d152570508a536b9c6804397abad29e6861ffce8
pbaldika <68334626+pbaldika@users.noreply.github.com>
2021-07-11 22:23:46 +0700

    Update package-details.php

--- package-details.php
+++ package-details.php
@@ -158,40 +158,38 @@ if (isset($_POST['submit'])) {
               <!-- Booking section  -->
               <section id="booking" class="contact">
                 <h2><b>Book Now</b></h2>
-                <form name="booking" method="POST" class="php-email-form" name="bookinsgform">
-                  <?php if ($error) { ?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
-                  <div class="form-group">
-                    <label>Your Name</label>
-                    <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                    <div class="validate"></div>
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label>Your Email</label>
-                      <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
-                      <div class="validate"></div>
-                    </div>
-                    <div class="form-group col-md-6">
-                      <label>Your Phone</label>
-                      <input type="text" name="phone" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter valid phone number" />
-                      <div class="validate"></div>
-                    </div>
-                  </div>
-                  <div class="form-row" class="form-group col-md-6">
-                    <div action="/action_page.php" class="form-group col-md-6">
-                      <label for="birthday">Date From:</label>
-                      <input type="date" id="fromdate" name="fromdate">
-                    </div>
+                <form method="post" role="form" name="enquiry">
 
-                    <div action="/action_page.php" class="form-group col-md-6">
-                      <label for="todate">Date To:</label>
-                      <input type="date" id="todate" name="todate">
-                    </div>
-                
-                  </div>
-                  <br>
-                  <div class="text-center"><button type="submit" name="submit">Create Booking</button></div>
-                </form>
+              <?php if ($error) { ?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
+
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+                <div class="validate"></div>
+              </div>
+              <div class="mb-3">
+                <div class="loading">Loading</div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit" name="submit1">Send Message</button></div>
+            </form>
               </section>
               <!-- Booking section  -->
 
