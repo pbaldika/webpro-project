4f8719ba65bdc0556a1dacd6730bad2fa4a367ca
dhioridhola <86360923+dhioridhola@users.noreply.github.com>
2021-07-11 17:19:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -103,7 +103,6 @@ include('assets/includes/config.php');
   <!--  
   <section id="featured-services" class="featured-services section-bg">
     <div class="container">
-
       <div class="row no-gutters">
         <div class="col-lg-4 col-md-6">
           <div class="icon-box">
@@ -127,7 +126,6 @@ include('assets/includes/config.php');
           </div>
         </div>
       </div>
-
     </div>
   </section><!-- End Featured Services Section -->
 
@@ -267,43 +265,7 @@ include('assets/includes/config.php');
 
       <div class="row no-gutters">
 
-        <div class="col-lg-4 col-md-6 content-item">
-          <span>01</span>
-          <h4>Lorem Ipsum</h4>
-          <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
-        </div>
-
-        <div class="col-lg-4 col-md-6 content-item">
-          <span>02</span>
-          <h4>Repellat Nihil</h4>
-          <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
-        </div>
-
-        <div class="col-lg-4 col-md-6 content-item">
-          <span>03</span>
-          <h4> Ad ad velit qui</h4>
-          <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
-        </div>
-
-        <div class="col-lg-4 col-md-6 content-item">
-          <span>04</span>
-          <h4>Repellendus molestiae</h4>
-          <p>Inventore quo sint a sint rerum. Distinctio blanditiis deserunt quod soluta quod nam mider lando casa</p>
-        </div>
-
-        <div class="col-lg-4 col-md-6 content-item">
-          <span>05</span>
-          <h4>Sapiente Magnam</h4>
-          <p>Vitae dolorem in deleniti ipsum omnis tempore voluptatem. Qui possimus est repellendus est quibusdam</p>
-        </div>
-
-        <div class="col-lg-4 col-md-6 content-item">
-          <span>06</span>
-          <h4>Facilis Impedit</h4>
-          <p>Quis eum numquam veniam ea voluptatibus voluptas. Excepturi aut nostrum repudiandae voluptatibus corporis sequi</p>
-        </div>
-
-      </div>
+        
 
     </div>
   </section><!-- End Why Us Section -->
@@ -318,9 +280,13 @@ include('assets/includes/config.php');
       </div>
 
       <div class="owl-carousel clients-carousel">
-        <img src="assets/img/clients/client-1.png" alt="">
-        <img src="assets/img/clients/client-2.png" alt="">
-        <img src="assets/img/clients/client-3.png" alt="">
+       
+        <img src="assets/img/clients/aston-logo-color.png" alt="">
+        <img src="assets/img/clients/novotel.png" alt="">
+        <img src="assets/img/clients/ritzcarlton.png" alt="">
+        <img src="assets/img/clients/accorshottel.png" alt="">
+        <img src="assets/img/clients/hyatt.png" alt="">
+        <img src="assets/img/clients/hilton.png" alt="">
       </div>
 
     </div>
